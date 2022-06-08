23f3f1b99a61a307d782ad590b21165a370f02ca
Alamin934 <islamalamin934@gmail.com>
2022-06-08 15:53:01 +0600

    create post api for send data to the database

--- index.js
+++ index.js
@@ -7,33 +7,47 @@ const port = process.env.PORT || 5000;
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ogrrwih.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
-app.get('/', (req, res) => {
-    res.send('Server Running successfully');
-});
 
 
 async function run() {
     try {
         await client.connect();
-        const database = client.db("insertDB");
-        const haiku = database.collection("haiku");
+        const database = client.db('carMechanic');
+        const servicesCollection = database.collection('services');
+
+        //POST Api
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await servicesCollection.insertOne(service);
+            res.json(result);
+            console.log(result);
+        })
 
 
-
-
-
-
-
-    } finally {
-        //   await client.close();
     }
+    finally {
+        //await client.close();
+    }
+
+
 }
 run().catch(console.dir);
 
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server Running successfully');
+});
 
 
 app.listen(port, () => {
